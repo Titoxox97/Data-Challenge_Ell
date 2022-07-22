d0056f6c9d1b82a3ab45f26288da99b9ec6ffc2d
Tito <matiasahrensdorf@gmail.com>
2022-07-22 14:58:09 -0400

    importing pandas and making data frames

--- work.py
+++ work.py
@@ -1,2 +1,5 @@
-print("Hello World")
-print
+import pandas as pd
+
+#
+df = pd.read_csv("C:/Users/matia/OneDrive/Desktop/Ellevation-Data-Question/sample-mcas-processed.csv")
+df2 = pd.read_csv("C:/Users/matia/OneDrive/Desktop/Ellevation-Data-Question/sample-mcas.csv")
