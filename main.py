badbfa7a1d1189f23edf35be507790e0ec34a201
Tito <matiasahrensdorf@gmail.com>
2022-07-24 18:04:57 -0400

    clean code up and attempt to run

--- main.py
+++ main.py
@@ -1,69 +1,69 @@
 import pandas as pd
 import csv
 
-def fileTransformer(filepath, filename, newfilepath):
+def filetransformer(filepath, filename, newfilepath):
     print('File starting')
-    filepath_filename = filepath +'/' + filename
-    File1 = pd.read_csv(filepath_filename)
+    filep_filen = filepath +'/' + filename
+    File1 = pd.read_csv(filep_filen)
     File1 = File1.to_dict('records')
 
 # Create data set out of dictionaries that will be used to loop through the data and apply conditions
 
-testds = [{'TestType': 'MCAS ELA', 'Date': '04-01', 'Subject': 'Ela', 'Performance': i['eperf2'], 'Scaled Score': i['escaleds'], 'CPI': i['ecpi']},
-          {'TestType': 'MCAS Math', 'Date': '05-01', 'Subject': 'Math', 'Performance': i['mperf2'], 'Scaled Score': i['mscaleds'], 'CPI': i['mcpi']},
-          {'TestType': 'MCAS Science', 'Date': '06-01', 'Subject': 'Science', 'Performance': i['sperf2'], 'Scaled Score': i['sscaleds'], 'CPI': i['scpi']}]
+    dataset = []
+    for i in File1:
+        testds = [{'TestType': 'MCAS ELA', 'Date': '04-01', 'Subject': 'Ela', 'Performance': i['eperf2'], 'Scaled Score': i['escaleds'], 'CPI': i['ecpi']},
+                  {'TestType': 'MCAS Math', 'Date': '05-01', 'Subject': 'Math', 'Performance': i['mperf2'], 'Scaled Score': i['mscaleds'], 'CPI': i['mcpi']},
+                  {'TestType': 'MCAS Science', 'Date': '06-01', 'Subject': 'Science', 'Performance': i['sperf2'], 'Scaled Score': i['sscaleds'], 'CPI': i['scpi']}]
 
 # Create loop within a loop to filter through conditions and grab the data we are calling
 
-dataset = []
-for i in File1:
-    for j in testds:
-        newrow = {}
-        newrow["NCESID"] = 373737
-        newrow['StudentLocalId'] = None
-        newrow['StudentTestId'] = i['sasid']
-        newrow['StudentGradeLevel'] = i['stugrade']
-        newrow['TestDate'] = j['Date']
-        newrow['TestName'] = 'Macs'
-        newrow['TestTypeName'] = j['Name']
-        newrow['TestSubjectName'] = j['Subject']
-        newrow['TestGradeLevel'] = i['stugrade']
-        newrow['Score1Label'] = 'Performance Level'
-        newrow['Score1Type'] = 'Level'
+        for j in testds:
+            newrow = {}
+            newrow["NCESID"] = 373737
+            newrow['StudentLocalId'] = None
+            newrow['StudentTestId'] = i['sasid']
+            newrow['StudentGradeLevel'] = i['stugrade']
+            newrow['TestDate'] = j['Date']
+            newrow['TestName'] = 'Macs'
+            newrow['TestTypeName'] = j['Name']
+            newrow['TestSubjectName'] = j['Subject']
+            newrow['TestGradeLevel'] = i['stugrade']
+            newrow['Score1Label'] = 'Performance Level'
+            newrow['Score1Type'] = 'Level'
 
         # Performance score value conditions
 
-        if j['Performance'] == 'F':
-            newrow['Score1Value'] = '1-F'
-        elif j['Performance'] == 'W':
-            newrow['Score1Value'] = '2-W'
-        elif j['Performance'] == 'NI':
-            newrow['Score1Value'] = '3-NI'
-        elif j['Performance'] == 'P':
-            newrow['Score1Value'] = '4-P'
-        elif j['Performance'] == 'A':
-            newrow['Score1Value'] = '5-A'
-        elif j['Performance'] == 'P+':
-            newrow['Score1Value'] = '6-P+'
-        else:
-            newrow['Score1Value'] = None
-        newrow['Score2Label'] = 'Scaled Score'
-        newrow['Score2Type'] = 'Scale'
-        if j['ScaledScore'] ==' ':
-            newrow['Score2Value'] = None
-        else:
-            newrow['Score2Value'] = j['ScaledScore']
-        newrow['Score3Label'] = 'CPI'
-        newrow['Score3Type'] = 'Scale'
-        if j['CPI'] == ' ':
-            newrow['Score3Value'] = None
-        else:
-            newrow['Score3Value'] = j['CPI']
-        dataset.append(newrow)
+            if j['Performance'] == 'F':
+                newrow['Score1Value'] = '1-F'
+            elif j['Performance'] == 'W':
+                newrow['Score1Value'] = '2-W'
+            elif j['Performance'] == 'NI':
+                newrow['Score1Value'] = '3-NI'
+            elif j['Performance'] == 'P':
+                newrow['Score1Value'] = '4-P'
+            elif j['Performance'] == 'A':
+                newrow['Score1Value'] = '5-A'
+            elif j['Performance'] == 'P+':
+                newrow['Score1Value'] = '6-P+'
+            else:
+                newrow['Score1Value'] = None
+            newrow['Score2Label'] = 'Scaled Score'
+            newrow['Score2Type'] = 'Scale'
+            if j['ScaledScore'] == ' ':
+                newrow['Score2Value'] = None
+            else:
+                newrow['Score2Value'] = j['ScaledScore']
+            newrow['Score3Label'] = 'CPI'
+            newrow['Score3Type'] = 'Scale'
+            if j['CPI'] == ' ':
+                newrow['Score3Value'] = None
+            else:
+                newrow['Score3Value'] = j['CPI']
+            dataset.append(newrow)
 
-df = pd.DataFrame(dataset)
-df.to_csv(newfilepath + '/' + filename[:-4] + '-processed.csv')
-return df
+    df = pd.DataFrame(dataset)
+    df.to_csv(newfilepath + '/' + filename[:-4] + '-processed.csv')
+    return df
 
 
 
