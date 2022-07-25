c9d68937d6303f1a01622ffb98ec226d10ef86de
Tito <matiasahrensdorf@gmail.com>
2022-07-25 14:37:01 -0400

    create new var in inputs_outputs.py to coincide with changes made yesterday in main

--- inputs_outputs.py
+++ inputs_outputs.py
@@ -2,8 +2,11 @@
 
 from main import filetransformer
 
+
 inputfile_path = "C:/Users/matia/OneDrive/Desktop/Ellevation-Data-Question" # path for input files
 inputfile_name = "sample-mcas.csv" # Create list of file names
 outputfile_path = "C:/Users/matia/function_output" # path where output files will be stored
 
-dataset = filetransformer(inputfile_path, inputfile_name, outputfile_path)
+list = [inputfile_path, inputfile_name, outputfile_path]
+
+dataset = filetransformer(list)
